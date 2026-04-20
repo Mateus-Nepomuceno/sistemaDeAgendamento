0e892235198d7ceaac6b90f411942f1b7b8ac451
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2026-04-20 14:31:25 -0300

    refactor: desacoplando funcionarios de anotacoes

---
+++ anotacoes/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Anotacao(models.Model):
+    titulo = models.CharField(max_length=200, verbose_name='Título')
+    descricao = models.TextField(verbose_name='Descrição')
+    link = models.URLField(blank=True, null=True, verbose_name='Link')
+    
+    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name='Data de Criação')
+    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name='Última Atualização')
+    
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='anotacoes')
+    
+    class Meta:
+        verbose_name = 'Anotação'
+        verbose_name_plural = 'Anotações'
+        ordering = ['-data_criacao']
+
+    def __str__(self):
+        return self.titulo
+
+
+class Funcionario(models.Model):    
+    TIPO_CHOICES = [
+        ('docente','Docente'),('tecnico','Tecnico'),
+    ]
+
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    cargo = models.CharField(max_length=100, verbose_name='Cargo')
+    processo=models.CharField(max_length=100,blank=True,null=True)
+    ano_avaliado=models.DateField(blank=True,null=True)
+    matricula=models.CharField(max_length=50,blank=True,null=True)
+    proxima_progressao=models.DateField(blank=True,null=True)
+    status=models.CharField(max_length=50,choices=[('ativo','Ativo'),('inativo','Inativo'),('em_progressao','Em Progressao')], default='ativo')
+    observacoes=models.TextField(blank=True,null=True)
+    tipo=models.CharField(max_length=20,choices=TIPO_CHOICES)
+    email = models.EmailField(blank=True, null=True, verbose_name='Email')
+    telefone = models.CharField(max_length=20, blank=True, null=True, verbose_name='Telefone')
+    data_contratacao = models.DateField(blank=True, null=True, verbose_name='Data de Contratação')
+    
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+        ordering = ['nome']
+
+    def __str__(self):
+        return f"{self.nome} ({self.tipo})"
---
+++ anotacoes/views.py
@@ -0,0 +1,63 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.db.models import Q
+from .models import Anotacao
+
+
+class AnotacaoListView(LoginRequiredMixin, ListView):
+    model = Anotacao
+    template_name = 'anotacoes/lista.html'
+    context_object_name = 'anotacoes'
+
+    def get_queryset(self):
+        lista = Anotacao.objects.filter(usuario=self.request.user)
+        
+        busca = self.request.GET.get('q')
+        if busca:
+            lista = lista.filter(
+                Q(titulo__icontains=busca) |
+                Q(descricao__icontains=busca)
+            )
+        
+        ordenar = self.request.GET.get('ordenar')
+        if ordenar == 'titulo':
+            lista = lista.order_by('titulo')
+        elif ordenar == 'titulo_desc':
+            lista = lista.order_by('-titulo')
+        elif ordenar == 'data':
+            lista = lista.order_by('data_criacao')
+        else:
+            lista = lista.order_by('-data_criacao')
+        
+        return lista
+
+
+class AnotacaoCreateView(LoginRequiredMixin, CreateView):
+    model = Anotacao
+    template_name = 'anotacoes/criar.html'
+    fields = ['titulo', 'descricao', 'link']
+    success_url = reverse_lazy('anotacao_lista')
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super().form_valid(form)
+
+
+class AnotacaoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Anotacao
+    template_name = 'anotacoes/editar.html'
+    fields = ['titulo', 'descricao', 'link']
+    success_url = reverse_lazy('anotacao_lista')
+
+    def get_queryset(self):
+        return Anotacao.objects.filter(usuario=self.request.user)
+
+
+class AnotacaoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Anotacao
+    template_name = 'anotacoes/excluir.html'
+    success_url = reverse_lazy('anotacao_lista')
+
+    def get_queryset(self):
+        return Anotacao.objects.filter(usuario=self.request.user)
