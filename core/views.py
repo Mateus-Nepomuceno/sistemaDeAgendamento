699b465e534a38796f6b04e1d44bc7963b711fa9
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2026-05-02 02:50:26 -0300

    feat: calendario de demandas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+import calendar
 from django.views.generic import TemplateView
 from cadastros.models import Funcionario
 from django.utils import timezone
@@ -15,8 +16,16 @@ class IndexView(TemplateView):
                 proxima_progressao__gte=hoje,
                 proxima_progressao__month=hoje.month,
                 proxima_progressao__year=hoje.year
-            ).exclude(
-                status='FI'
-            ).order_by('proxima_progressao')[:5]
+            ).exclude(status='FI').order_by('proxima_progressao')[:4]
+            
+            _, num_dias = calendar.monthrange(hoje.year, hoje.month)
+            
+            dias_com_progressao = Funcionario.objects.filter(
+                proxima_progressao__month=hoje.month,
+                proxima_progressao__year=hoje.year
+            ).exclude(status='FI').values_list('proxima_progressao__day', flat=True)
+            
+            lista['dias_calendario'] = range(1, num_dias + 1) 
+            lista['dias_destaque'] = list(dias_com_progressao)
             
         return lista
