a7c0fe89018b863bce3ae0511589cbd19cbe12c2
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2026-05-02 01:56:40 -0300

    feat: lista de demandas recentes

--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from cadastros.models import Funcionario
+from django.utils import timezone
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        lista = super().get_context_data(**kwargs)
+        
+        if self.request.user.is_authenticated:
+            hoje = timezone.now().date()
+            
+            lista['funcionarios'] = Funcionario.objects.filter(
+                proxima_progressao__gte=hoje,
+                proxima_progressao__month=hoje.month,
+                proxima_progressao__year=hoje.year
+            ).exclude(
+                status='FI'
+            ).order_by('proxima_progressao')[:5]
+            
+        return lista
